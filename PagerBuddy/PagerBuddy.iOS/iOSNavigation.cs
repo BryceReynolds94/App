4ad5d0602854d5de8d1a9015f3a13314317582b1
Max Bartunik <max.bartunik@fau.de>
2021-01-10 15:02:26 +0100

    Changed HomeScreen layout stretch behaviour

--- PagerBuddy/PagerBuddy.iOS/iOSNavigation.cs
+++ PagerBuddy/PagerBuddy.iOS/iOSNavigation.cs
@@ -1,5 +1,6 @@
 ﻿using Foundation;
 using PagerBuddy.Interfaces;
+using PagerBuddy.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,8 @@ using UIKit;
 [assembly: Xamarin.Forms.Dependency(typeof(PagerBuddy.iOS.iOSNavigation))] //register for dependency service as platform-specific code
 namespace PagerBuddy.iOS {
     class iOSNavigation : INavigation {
+
+        private static NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
         public bool isTelegramInstalled() {
             //TODO: IOS Implementation
             //https://stackoverflow.com/questions/41545283/how-to-check-app-is-installed-or-not-in-phone
@@ -16,7 +19,9 @@ namespace PagerBuddy.iOS {
         }
 
         public void navigateNotificationPolicyAccess() {
-            //TODO: IOS Implementation
+            //This is only used in android
+            //TODO: Check if equivalent call for iOS necessary (https://developer.apple.com/documentation/usernotifications/asking_permission_to_use_notifications)
+            Logger.Error("NavigateNotificationPolicyAccess called on iOS. This should never happen.");
             throw new NotImplementedException();
         }
 
@@ -25,14 +30,14 @@ namespace PagerBuddy.iOS {
             throw new NotImplementedException();
         }
 
-        public void navigateTelegramChat(int chatID) {
-            //TODO: IOS Implementation
+        public void navigateTelegramChat(int chatID, TelegramPeer.TYPE type) {
+            //TODO: IOS Implementation 
             throw new NotImplementedException();
         }
 
         public void quitApplication() {
-            //TODO: IOS Implementation
-            throw new NotImplementedException();
+            //This cannot be done in iOS - only Home button is valid to close app
+            //TODO: Check other implementation of closing alert (probably no alert screen at all with iOS)
         }
     }
-}
+} 
